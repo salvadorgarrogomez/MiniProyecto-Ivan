79d8c1dbe0b729f497f6076306d76008063fa097
Salva G.G <125753997+salvadorgarrogomez@users.noreply.github.com>
2024-06-06 11:31:30 +0200

    Add files via upload

--- src/custom_addons/import_csv/wizards/importar_csv.py
+++ src/custom_addons/import_csv/wizards/importar_csv.py
@@ -2,6 +2,7 @@ from odoo import models, fields, api
 from odoo.exceptions import UserError
 import csv
 import base64
+import re
 from io import StringIO
 
 class ImportContactsWizard(models.TransientModel):
@@ -21,7 +22,12 @@ class ImportContactsWizard(models.TransientModel):
         header = next(csv_reader)
         duplicate_external_ids = []
         errors = []
-        for row_number, row in enumerate(csv_reader, start=2):  # Empieza la lectura para insertar datos despues de la segunda linea
+
+        # Definición de expresiones regulares
+        numeric_regex = re.compile(r'^\d+$')
+        alphabetic_regex = re.compile(r'^[a-zA-Z\s]+$')
+
+        for row_number, row in enumerate(csv_reader, start=2):  # Empieza la lectura para insertar datos después de la segunda línea
             # Se quitan los espacios en blanco
             row = list(map(str.strip, row))
             # Verificar si la fila tiene el número correcto de columnas
@@ -29,17 +35,43 @@ class ImportContactsWizard(models.TransientModel):
                 errors.append(f"Row {row_number}: Incorrect number of columns.")
                 continue
             vals = dict(zip(header, row))
-            # Se ignoran las lineas en blanco
+            # Se ignoran las líneas en blanco
             if not any(vals.values()):
                 continue
+
             external_id = vals.get('external_id')
             name = vals.get('name')
+            phone = vals.get('phone')
+            email = vals.get('email')
+            street = vals.get('street')
+            city = vals.get('city')
+            zip_code = vals.get('zip')
+            country = vals.get('country')
+
             # Se comprueba si los campos esenciales están presentes
             if not name:
                 errors.append(f"Row {row_number}: Missing 'name'.")
                 continue
+            
+            if name and not alphabetic_regex.match(name):
+                errors.append(f"Row {row_number}: Name must contain only alphabetic characters.")
+                continue
+
+            # Validaciones de campos específicos
+            if phone and not numeric_regex.match(phone):
+                errors.append(f"Row {row_number}: Phone must be numeric.")
+                continue
+
+            if zip_code and not numeric_regex.match(zip_code):
+                errors.append(f"Row {row_number}: ZIP must be numeric.")
+                continue
+
+            if city and not alphabetic_regex.match(city):
+                errors.append(f"Row {row_number}: City must contain only alphabetic characters.")
+                continue
+
             if external_id:
-                # Comprobacion de si existe el id externo
+                # Comprobación de si existe el id externo
                 existing_partner = self.env['res.partner'].search([('external_id', '=', external_id)], limit=1)
                 if existing_partner:
                     duplicate_external_ids.append(external_id)
@@ -47,13 +79,13 @@ class ImportContactsWizard(models.TransientModel):
             try:
                 self.env['res.partner'].create({
                     'name': name,
-                    'phone': vals.get('phone', '').strip() if vals.get('phone') else '',
-                    'email': vals.get('email', '').strip() if vals.get('email') else '',
-                    'street': vals.get('street', '').strip() if vals.get('street') else '',
-                    'city': vals.get('city', '').strip() if vals.get('city') else '',
-                    'zip': vals.get('zip', '').strip() if vals.get('zip') else '',
-                    'external_id': external_id.strip(),
-                    'country_id': self.env['res.country'].search([('name', '=', vals.get('country', '').strip())], limit=1).id if vals.get('country') else None,
+                    'phone': phone if phone else '',
+                    'email': email if email else '',
+                    'street': street if street else '',
+                    'city': city if city else '',
+                    'zip': zip_code if zip_code else '',
+                    'external_id': external_id.strip() if external_id else '',
+                    'country_id': self.env['res.country'].search([('name', '=', country.strip())], limit=1).id if country else None,
                 })
             except Exception as e:
                 errors.append(f"Row {row_number}: Error creating record - {str(e)}")
